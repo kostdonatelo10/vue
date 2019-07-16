3981614f8b6357a8aae02b23b277db9951c34525
brayan <brayanjulinho@gmail.com>
2019-07-16 12:20:26 -0500

    mi primera vez

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="brayan"/>
+    <hi/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import hi from './components/hi'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,hi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/hi.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <label>hola</label>
+    </div>
+</template>
